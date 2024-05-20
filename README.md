2a5349e03bf714a3dd9434ed1b6314fadeeea4f8
Martín Perez <100448340+TinchoGithub@users.noreply.github.com>
2024-05-20 09:54:46 -0300

    Subo proyecto consumir api a repo

---
+++ README.md
@@ -0,0 +1,55 @@
+![Banner](./assets/img/banner.png)
+
+<div align='center'>
+    <a href="https://tinchogithub.github.io/chistes-para-programadores/" target="_blank">
+          <img  src="https://img.shields.io/badge/VER_DEMO-f0ad4e?style=for-the-badge&logo=vercel&logoColor=%23343B4E"/>
+       </a>
+</div>
+
+## Descripción
+
+Este proyecto es un desafío del programa Codo a Codo 4.0. El objetivo del proyecto es consumir una API y mostrar contenido en el front-end. En este caso, se consume la API [JokeAPI](https://v2.jokeapi.dev/joke) para mostrar chistes para programador@s al presionar un botón.
+
+## Tecnologías
+
+Este proyecto utiliza las siguientes tecnologías:
+
+![HTML Badge](https://img.shields.io/badge/HTML-5-orange)
+![CSS Badge](https://img.shields.io/badge/CSS-3-blue)
+![JavaScript Badge](https://img.shields.io/badge/JavaScript-ES6-yellow)
+
+## Funcionalidad
+
+Al presionar el botón en la página principal, se realiza una solicitud a la API de chistes y se muestra un chiste relacionado con la programación en el front-end. Dependiendo del tipo de chiste recibido (de una parte o de dos partes), se presenta de manera adecuada en la interfaz de usuario.
+
+## Uso
+
+1. Clona este repositorio en tu máquina local.
+2. Abre el archivo `index.html` en tu navegador.
+3. Haz clic en el botón para obtener un chiste y mostrarlo en la página.
+
+## Instalación
+
+Para correr este proyecto localmente, sigue estos pasos:
+
+1. Clona el repositorio:
+   ```sh
+   git clone https://github.com/tu-usuario/chistes-para-programadores.git
+
+2. Navega al directorio del proyecto:
+   ```
+   cd chistes-para-programadores
+3. Abre index.html en tu navegador preferido.
+
+## Contribución
+Las contribuciones son bienvenidas. Si deseas contribuir, por favor sigue estos pasos:
+
+1. Haz un fork del proyecto.
+2. Crea una nueva rama (feature/nueva-funcionalidad).
+3. Realiza tus cambios.
+4. Envía un pull request.
+
+## Licencia
+Este proyecto está bajo la licencia MIT. Para más detalles, revisa el archivo LICENSE.
+
+¡Gracias por visitar este proyecto y disfrutar de los chistes para programadores! 🚀
---
+++ controller/script.js
@@ -0,0 +1,71 @@
+const botonRequest = document.getElementById('boton-chiste').addEventListener('click', obtenerChiste);
+const imagen = "../assets/img/bmo.gif";
+
+/* ASYNC AWAIT */
+
+async function obtenerChiste() {
+    try {
+        const response = await fetch("https://v2.jokeapi.dev/joke/Programming?lang=es&idRange=1-6");
+        const data = await response.json();
+        console.log(data);
+
+        const contenido = document.getElementById("contenido");
+        const botonResponse = document.getElementById("boton-respuesta");
+
+        if (data.type === "twopart") {
+            contenido.innerHTML = `<p><strong>${data.setup}</strong></p>`;
+            botonResponse.style.display = "inline";
+            botonResponse.onclick = () => mostrarRespuesta(data.delivery);
+        } else if (data.type === "single") {
+            contenido.innerHTML = `<p>${data.joke}</p>
+            <img style="width: 100px;" src="${imagen}" alt="Imagen">`;
+            botonResponse.style.display = "none";
+        }
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+function mostrarRespuesta(delivery) {
+    const contenido = document.getElementById("contenido");
+    contenido.innerHTML += `
+    <p class="text-warning"><strong>${delivery}</strong> </p>
+    <img style="width: 100px;" src="${imagen}" alt="Imagen">`;
+    document.getElementById("boton-respuesta").style.display = "none";
+    document.getElementById("boton-chiste").style.display = "inline";
+
+}
+
+/* FETCH */
+
+//  function obtenerChiste() {
+//      fetch("https://v2.jokeapi.dev/joke/Programming?lang=es&idRange=1-6")
+//          .then(response => response.json())
+//          .then(data => {
+//              console.log(data);
+
+//              const contenido = document.getElementById("contenido");
+//              const botonResponse = document.getElementById("boton-respuesta");
+
+//              if (data.type === "twopart") {
+//                  let setup = data.setup;
+//                  let delivery = data.delivery;
+
+//                  contenido.innerHTML = `<p><strong>${setup}</strong></p>`;
+
+//                  botonResponse.style.display = "inline";
+//                  botonResponse.onclick = () => mostrarRespuesta(delivery);
+//              } else if (data.type === "single") {
+//                  let joke = data.joke;
+
+//                  contenido.innerHTML = `<p>${joke}</p>
+//                   <img style="width: 100px;" src="${imagen}" alt="Imagen">`;
+//                  botonResponse.style.display = "none";
+//              }
+//          })
+//          .catch(error => {
+//              console.log(error);
+//          });
+//  }
+
+
